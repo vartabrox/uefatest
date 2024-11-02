a06d012bfea037b23a10be54c37d6cbba0f930ce
Sergey <vartabrox@gmail.com>
2024-11-02 15:52:21 +0300

    uefatest commit

---
+++ 1_hello_world/Program.cs
@@ -0,0 +1,9 @@
+class MyBestApp
+{
+ static void Main() // The Main is the entry point of the program
+ {
+ System.Console.WriteLine("Hello NextNode"); // Using the method WriteLine, i was able to output the string "Hello NextNode"
+ System.Console.WriteLine("Next " + "Node");
+ }
+}
+// I also tested the comments
